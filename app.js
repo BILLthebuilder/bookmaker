1af881b7a997d3bcd6febac492784b49c8b3e427
Bill <billkariri@gmail.com>
2019-02-15 21:30:48 +0300

    add function to delete bookmark

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 (function () {
     let myForm = document.getElementById('myForm');
+    
     function saveBookmark(e) {
         e.preventDefault();
         let siteName = document.getElementById('siteName').value;
@@ -10,8 +11,6 @@
             name: siteName,
             url: urlName
         }
-
-
         if (!siteName && !urlName) {
             alert('Please fill in the fields');
         }
@@ -31,7 +30,7 @@
 
     }
 
-    function fetchBookmarks(){
+    function fetchBookmarks() {
         let bookmarks = JSON.parse(localStorage.getItem('values'));
         let results = document.getElementById('results');
 
@@ -39,20 +38,34 @@
         //     let name = value.siteName;
         //     results.innerHTML = `${name}`;
         // });
-        for (let i=0; i<bookmarks.length; i++){
+        for (let i = 0; i < bookmarks.length; i++) {
             let name = bookmarks[i].name;
             let url = bookmarks[i].url;
             results.innerHTML += `<div class="card card-body"> 
                                 <h3>${name}
                                 <a class="btn btn-info" target="_blank" href="${url}">Visit</a>
-                                <a onClick="deleteBookmark(${url})" class="btn btn-danger" href="#">Delete</a>
+                                <a class="btn btn-danger" href="#">Delete</a>
 
                                 </h3>
                                     </div>`;
+            
         }
-        
+
+    }
+
+    function deleteBookmark(url) {
+        let bookmarks = JSON.parse(localStorage.getItem('values'));
+
+        for (let i = 0; i < bookmarks.length; i++) {
+            if (bookmarks[i].url == url) {
+                bookmarks.splice(i, 1);
+            }
+        }
+        localStorage.setItem('values', JSON.stringify(bookmarks));
     }
     window.onload = fetchBookmarks();
+    // let btn = document.querySelector('.btn-danger');
+    // btn.addEventListener('click', deleteBookmark(url));
     myForm.addEventListener('submit', saveBookmark);
 
 })();
