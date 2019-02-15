520bc6ce21581b3e9089c081fa68ddac6471f403
Bill <billkariri@gmail.com>
2019-02-15 18:57:52 +0300

    create functionality to fetch a bookmark

--- app.js
+++ app.js
@@ -31,7 +31,28 @@
 
     }
 
-    
+    function fetchBookmarks(){
+        let bookmarks = JSON.parse(localStorage.getItem('values'));
+        let results = document.getElementById('results');
+
+        // bookmarks.forEach(value=>{
+        //     let name = value.siteName;
+        //     results.innerHTML = `${name}`;
+        // });
+        for (let i=0; i<bookmarks.length; i++){
+            let name = bookmarks[i].name;
+            let url = bookmarks[i].url;
+            results.innerHTML += `<div class="card card-body"> 
+                                <h3>${name}
+                                <a class="btn btn-info" target="_blank" href="${url}">Visit</a>
+                                <a onClick="deleteBookmark(${url})" class="btn btn-danger" href="#">Delete</a>
+
+                                </h3>
+                                    </div>`;
+        }
+        
+    }
+    window.onload = fetchBookmarks();
     myForm.addEventListener('submit', saveBookmark);
 
 })();
